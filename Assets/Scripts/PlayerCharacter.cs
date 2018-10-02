d5be325cba51ac71de1e9b7ddb7c0a01d928b90c
heyitsamanie <42983780+heyitsamanie@users.noreply.github.com>
2018-10-02 15:10:16 -0500

    #4: Creating a Jump!

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -12,32 +12,38 @@ public class PlayerCharacter : MonoBehaviour
     private float maxSpeed = 5;
 
     [SerializeField]
+    private float jumpForce = 10;
+
+    [SerializeField]
     private Rigidbody2D rb2d;
 
     private float horizontalInput;
 
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
 	// Update is called once per frame
 	void Update ()
     {
         horizontalInput = Input.GetAxis("Horizontal");
 
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+        }
+
 	}
 
-    private void FixedUpdate() //make physics calls in fixed update 
+    private void FixedUpdate() 
     {
-
-        rb2d.AddForce(Vector2.right * horizontalInput * acceleractionForce);
-
-        Vector2 clampedVelocity = rb2d.velocity;
-        clampedVelocity.x = Mathf.Clamp(rb2d.velocity.x, -maxSpeed, maxSpeed);
-        rb2d.velocity = clampedVelocity;
+        Move();
 
     }
 
+    private void Move()
+    {
+        rb2d.AddForce(Vector2.right * horizontalInput * acceleractionForce);
+        Vector2 clampedVelocity = rb2d.velocity;
+        clampedVelocity.x = Mathf.Clamp(rb2d.velocity.x, -maxSpeed, maxSpeed);
+        rb2d.velocity = clampedVelocity;
+    }
 }
