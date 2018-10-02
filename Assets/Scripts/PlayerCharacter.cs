1001ec8562fbcfbe87d6cfef939852eb7563eb90
heyitsamanie <42983780+heyitsamanie@users.noreply.github.com>
2018-10-02 14:34:55 -0500

    #1: Character/Level Setup & Basic Movement

---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed = 5;
+
+    [SerializeField]
+    private Rigidbody2D rb2d;
+
+    private float horizontalInput;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+	}
+
+    private void FixedUpdate() //make physics calls in fixed update 
+    {
+
+        rb2d.AddForce(Vector2.right * horizontalInput * speed);
+
+    }
+
+}
